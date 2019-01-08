47b22665b80319808fa3ac796052f38a8c3f08c7
LeCuay <suso.becerra98@gmail.com>
2019-01-08 01:17:19 +0000

    Added basic stuff

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,50 +9,80 @@
     <!-- Open Graph general (Facebook, Pinterest & Google+) -->
     <meta property="og:title" content="Script Hub - Documentación y Bots">
     <meta property="og:description" content="Programación de bots con Discord en español.">
-    <meta property="og:image" content="./docs/assets/server-ui/logo-cord-re.png">
+    <meta property="og:image" content="/assets/server-ui/logo-cord-re.png">
     <meta property="og:url" content="https://scripthubteam.github.io/">
     <meta property="og:site_name" content="Script Hub">
     <meta property="og:type" content="website">
 
     <!-- Motor de búsqueda -->
     <meta name="description" content="Script Hub - Documentación y Bots">
-    <meta name="image" content="./docs/assets/server-ui/logo-cord-re.png">
+    <meta name="image" content="/assets/server-ui/logo-cord-re.png">
     <link rel="canonical" href="https://scripthubteam.github.io/" />
 
     <!-- Schema.org Google -->
     <meta itemprop="name" content="Script Hub - Documentación y Bots">
     <meta itemprop="description" content="Programación de bots con Discord en español.">
-    <meta itemprop="image" content="./docs/assets/server-ui/assets/logo-cord-re.png">
+    <meta itemprop="image" content="/assets/server-ui/assets/logo-cord-re.png">
 
     <!-- Twitter -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Script Hub - Documentación y Bots">
     <meta name="twitter:description" content="Programación de bots con Discord en español.">
-    <meta name="twitter:image" content="./docs/assets/server-ui/logo-cord-re.png">
+    <meta name="twitter:image" content="/assets/server-ui/logo-cord-re.png">
 
     <!-- Web Stuff -->
     <link rel="canonical" href="https://scripthubteam.github.io/" />
-    <link rel="shortcut icon" href="favicon.png">
+    <link rel="shortcut icon" href="/favicon.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
     <!-- Bootstrap Stuff -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
+    <!-- Personal Style -->
+    <link rel="stylesheet" href="/css/layout.css">
+
         <title>@yield('title')</title>
 </head>
 <body>
     <!-- Header Section -->
-    <header>
+    <header class="d-flex flex-column align-items-stretch justify-content-center p-3">
+        <ul class="nav nav-dark nav-fill align-items-center">
+            <li class="nav-item d-none d-sm-block justify-content-center">
+                <figure id="logo-scripthub">
+                    <img class="w-100" src="/assets/server-ui/logo-cord-raw.png" alt="Logo Script Hub Team">
+                </figure>
+            </li>
+            <li class="nav-item">
+                <a href="https://scripthubteam.github.io/docs/" target="_blank" class="nav-link btn btn-info text-light p-3">
+                    <span class="fa fa-book"></span> Documentación
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="https://www.patreon.com/scripthubteam" target="_blank" class="nav-link btn text-light p-3 ml-2">
+                    <span class="fab fa-patreon"></span> Patreon
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="https://discordapp.com/invite/VK2V7Yk" target="_blank" class="nav-link btn text-light p-3 ml-2">
+                    <span class="fab fa-discord"></span> Discord
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/register/" target="_blank" class="nav-link btn btn-secondary text-light p-3 ml-0 ml-sm-2 mt-2 mt-sm-0">
+                    <span class="fas fa-user"></span> Iniciar sesión
+                </a>
+            </li>
+        </ul>
         @yield('header_content')
     </header>
 
     <!-- Main Section -->
-    <main>
+    <main class="d-flex flex-column bg-white align-items-center justify-content-around h-100vh p-4">
         @yield('main_content')
     </main>
 
     <!-- Footer Section -->
-    <footer>
+    <footer class="d-flex flex-row">
         @yield('footer_content')
     </footer>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,20 @@
 @extends('layout')
 
 @section('title', "Script Hub - Documentación y Bots")
+
+@section('header_content')
+@stop
+
+@section('main_content')
+    <figure class="figure">
+        <img src="/assets/logo-bn.png" alt="Script Hub - Documentación y Bots" class="figure-img img-fluid">
+    </figure>
+    <h1 class="font-weight-light">Programación de bots con Discord en Español.</h1>
+    <blockquote class="blockquote text-center p-4">
+        <p class="font-italic font-weight-light">"Somos una comunidad de programadores que escribe guías, compartimos nuestras experiencias y ayudamos a responder dudas sobre programación."</p>
+        <footer class="blockquote-footer">Script Hub Team</footer>
+    </blockquote>
+@stop
+
+@section('footer_section')
+@endsection

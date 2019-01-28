bf9f10da43061674acf35c410f2a98fbe07a3201
LeCuay <suso.becerra98@gmail.com>
2019-01-28 00:05:41 +0000

    Fixed issues

--- database/factories/ScriptHubUsersFactory.php
+++ database/factories/ScriptHubUsersFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\ScriptHubUsers::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $faker->password,
         'description' => $faker->boolean(50) ? $faker->sentences(3, true) : null,
-        'avatar_url' => $faker->boolean(50) ? $faker->imageUrl : null,
+        'avatar_url' => $faker->boolean(50) ? $faker->imageUrl() : null,
         'discord_users_id' => factory(App\DiscordUsers::class)->create()->id,
     ];
 });
